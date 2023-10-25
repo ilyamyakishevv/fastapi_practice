e39deb56ea9ab4d6b19cddf87b4b848d9ec7582e
Ilya Myakishev <ilyamyakishev96@gmail.com>
2023-10-25 18:48:26 +0300

    http methods practice

--- app.py
+++ app.py
@@ -1,32 +1,34 @@
 import fastapi
+from fastapi import Request
 import pydantic_models
 import config
 import database
+import copy
 
 api = fastapi.FastAPI()
 
 
 fake_database = {'users':[
     {
-        "id":1,             
-        "name":"Anna",     
-        "nick":"Anny42",   
-        "balance": 15300   
+        "id":1,
+        "name":"Anna",
+        "nick":"Anny42",
+        "balance": 15300
      },
 
     {
-        "id":2,            
-        "name":"Dima",     
+        "id":2,
+        "name":"Dima",
         "nick":"dimon2319",
         "balance": 160.23
+     },
+    {
+        "id":3,
+        "name":"Vladimir",
+        "nick":"Vova777",
+        "balance": 200.1
      }
-    ,{
-        "id":3,             
-        "name":"Vladimir",  
-        "nick":"Vova777",    
-        "balance": "25000"
-     }
-],}
+    ], }
 
 
 @api.get('/get_info_by_user_id/{id:int}')
@@ -41,7 +43,7 @@ def get_user_balance_by_id(id):
 def get_total_balance(): 
     total_balance: float = 0.0
     for user in fake_database['users']:
-        total_balance += pydantic_models.User(**user).balance 
+        total_balance += pydantic_models.User(**user).balance
     return total_balance
 
 @api.get("/users/")
@@ -53,3 +55,29 @@ def read_user(user_id: str, query: str | None = None):
     if query: 
         return {'user_id': user_id, 'query': query}
     return {'user_id': user_id}
+
+
+
+@api.post('/user/create')
+def create_user(user: pydantic_models.User):
+    fake_database["users"].append(user)
+    return {"User created": user}
+
+
+
+@api.put('/user/{user_id}')
+def update_user(user_id: int, user: pydantic_models.User = fastapi.Body()):
+    for index, u in enumerate(fake_database['users']):
+        if u['id'] == user_id:
+            fake_database['users'][index] = user 
+            return user
+
+@api.delete('/user/{user_id}')
+def delete_usesr(user_id: int = fastapi.Path()): 
+    for index, u in enumerate(fake_database["users"]): 
+        if u['id'] == user_id: 
+            old_db = copy.deepcopy(fake_database)
+            del fake_database["users"][index]
+            return {"old db": old_db, 
+                    "new db": fake_database
+                    }
