e572b5464ef864ff519325b80cbfc201aed65627
Ilya Myaksihev <ilyamyakishev96@gmail.com>
2023-10-29 13:56:48 +0300

    added data in db

--- database/db.py
+++ database/db.py
@@ -1,1 +1,23 @@
-import pony
+from pony.orm import *
+import models
+
+db = Database()
+
+class User(db.Entity):
+    user_id = Required(str)
+    nick = Required(str)
+    age = Required(int)
+    wallets = Set('Wallet')
+
+
+class Wallet(db.Entity):
+    address = Required(str)
+    private_key = Required(str)
+    owner = Required(User)
+    
+    
+try:
+    db.bind(provider='sqlite', filename='/Users/mac/LearnPython/fastapi_practice/database/database.sqlite', create_db=True)
+    db.generate_mapping(create_tables=True)
+except Exception as Ex:
+    print(Ex)
