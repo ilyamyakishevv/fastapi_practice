1a46f77c1d1d278c43dec0c517e013b4253bf0b1
Илья Д. Мякишев <imyakishev@lovit.ru>
2023-10-30 16:51:50 +0300

    Models added

--- database/db.py
+++ database/db.py
@@ -1,23 +1,4 @@
-from pony.orm import *
-import models
+from models import *
 
-db = Database()
-
-class User(db.Entity):
-    user_id = Required(str)
-    nick = Required(str)
-    age = Required(int)
-    wallets = Set('Wallet')
-
-
-class Wallet(db.Entity):
-    address = Required(str)
-    private_key = Required(str)
-    owner = Required(User)
-    
-    
-try:
-    db.bind(provider='sqlite', filename='/Users/mac/LearnPython/fastapi_practice/database/database.sqlite', create_db=True)
-    db.generate_mapping(create_tables=True)
-except Exception as Ex:
-    print(Ex)
+db.bind(provider='sqlite', filename='database.sqlite', create_db=True)
+db.generate_mapping(create_tables=True)
---
+++ database/models.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+from pony.orm import *
+
+
+db = Database()
+
+
+class User(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    tg_ID = Required(int, unique=True)
+    nick = Optional(str)
+    created_date = Required(datetime)
+    wallet = Required('Wallet')
+    sended_transactions = Set('Transaction', reverse='sender')
+    received_transactions = Set('Transaction', reverse='receiver')
+
+
+class Wallet(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    user = Optional(User)
+    balance = Required(float, default="0.0")
+    private_key = Required(str, unique=True)
+    address = Required(str, unique=True)
+    transactions = Set('Transaction', reverse='sender_wallet')
+    received_transactions = Set('Transaction', reverse='receiver_wallet')
+
+
+class Transaction(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    sender = Optional(User, reverse='sended_transactions')
+    sender_wallet = Required(Wallet, reverse='transactions')
+    receiver = Optional(User, reverse='received_transactions')
+    receiver_wallet = Optional(Wallet, reverse='received_transactions')
+    sender_address = Optional(str)
+    receiver_address = Optional(str)
+    amount_bts_with_fe = Optional(float)
+    amount_btc_without_fee = Optional(float)
+    fee = Required(float)
+    date_of_transaction = Required(datetime)
+    tx_hash = Required(str, unique=True)
