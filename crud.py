ab8292ef6a4cd70e99c03c655d8776a0e3be7124
Илья Д. Мякишев <imyakishev@lovit.ru>
2023-10-31 11:49:50 +0300

    CRUD creating starts

--- crud.py
+++ crud.py
@@ -1,14 +1,45 @@
+import os
+import sys
 import datetime
-import pydantic_models
-import models
+import pydantic_models 
+from models import *
 import bit
-from config import WIF
+from db import *
+from config import WIF, ADR
+from pony.orm import db_session
 
-wallet = bit.PrivateKeyTestnet(WIF)
-print(f"Balance {wallet.get_balance()}")
-print(f"Address {wallet.address}")
-print(f"Private key {wallet.to_wif()}")
-print(f"All transactions {wallet.get_transactions()}")
+sys.path.append(os.getcwd())
+
+@db_session
+def create_wallet(user: pydantic_models.User = None, private_key: str = None, testnet: bool = False):
+    if not testnet:
+        raw_wallet = bit.Key() if not private_key else bit.Key(private_key)
+    else:
+        raw_wallet = bit.PrivateKeyTestnet() if not private_key else bit.PrivateKeyTestnet(private_key)
+    if user:
+        wallet = Wallet(user=user, private_key=raw_wallet.to_wif(), address=raw_wallet.address)
+    else:
+        wallet = Wallet(private_key=raw_wallet.to_wif(), address=raw_wallet.address)
+    flush()
+    return wallet
+
+
+@db_session
+def create_user(tg_id: int, nick: str = None):
+    if nick:
+        user = User(tg_ID=tg_id, nick=nick, create_date=datetime.now(), wallet=create_wallet())
+    else:
+        user = User(tg_ID=tg_id, created_date=datetime.now(), wallet=create_wallet())
+    flush()     
+    return user
+
+
+
+# wallet = bit.PrivateKeyTestnet(WIF)
+# print(f"Balance {wallet.get_balance()}")
+# print(f"Address {wallet.address}")
+# print(f"Private key {wallet.to_wif()}")
+# print(f"All transactions {wallet.get_transactions()}")
 
 # outputs = [("muh9DYMTWXfPEd9zPnfvCS1yX8hPLbkE9e", 0.000001, 'btc')]
 # transactions = wallet.send(outputs)
--- pydantic_models.py
+++ pydantic_models.py
@@ -1,5 +1,5 @@
 import pydantic
-import datetime
+from datetime import datetime
 
 
 class User(pydantic.BaseModel):
@@ -8,5 +8,31 @@ class User(pydantic.BaseModel):
     nick: str = None
     created_date: datetime
     wallet: "Wallet"
-    sended_transactions: list['Trasaction'] = None
-    received_transactions: list['Trasaction'] = None
+    sended_transactions: list['Transaction'] = None
+    received_transactions: list['Transaction'] = None
+
+
+class Transaction(pydantic.BaseModel): 
+    id: int 
+    sender: User = None 
+    reciever: User = None
+    sender_wallet: 'Wallet' = None
+    receiver_wallet: 'Wallet' = None
+    sender_address: str 
+    receiver_address: str 
+    amount_btc_with_fee: float
+    amount_btc_without_fee: float
+    fee: float
+    date_of_transaction: datetime
+    tx_hash: str 
+
+
+class Wallet(pydantic.BaseModel):
+    id: int 
+    user: User 
+    balance: float = 0.0
+    private_ker: str 
+    address: str 
+    sended_transactions: list[Transaction] = []
+    received_transactions: list[Transaction] = []
+
